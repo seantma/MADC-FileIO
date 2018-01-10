c376646d7d679b397aa00026e930c74362c350fe
Sean@benmac <tehsheng+benmac@umich.edu>
2018-01-10 12:21:00 -0500

    fixing executing find bug

--- madc_fs.sh
+++ madc_fs.sh
@@ -22,7 +22,7 @@ export SUBJECTS_DIR=/mnt/psych-bhampstelab/MADC/FSresults
 for SUBJ in ${subjIDs[@]}
 do
   cd ${DATADIR}/${SUBJ}
-  DCM=(find . -type f -name '*.1')
+  DCM=`find . -type f -name '*.1'`
 
   # log start time
   THEDATE=`date`
