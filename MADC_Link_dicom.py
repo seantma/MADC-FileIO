6aa975e049ddc9c6e9f41e8e8db81180c1c900d0
Sean@benmac <tehsheng+benmac@umich.edu>
2018-03-05 13:41:36 -0500

    init commit for mri dicom link

---
+++ MADC_Link_dicom.py
@@ -0,0 +1,8 @@
+# @Author: Sean Ma <tehsheng>
+# @Date:   2018-03-05T13:39:06-05:00
+# @Email:  tehsheng@umich.edu
+# @Filename: MADC_Link_dicom.py
+# @Last modified by:   tehsheng
+# @Last modified time: 2018-03-05T13:39:52-05:00
+
+# Link s0000x dicom folders to actual modalities to allow further processing
