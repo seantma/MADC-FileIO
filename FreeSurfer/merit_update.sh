f1b159f086ecd2d0eacc76d2ce3cbbface2c46ad
Sean@hplaptop <tehsheng+hplaptop@umich.edu>
2018-03-14 16:59:52 -0400

    move merit update to Merit folder

--- FreeSurfer/merit_update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# setting up environment
-export SUBJECTS_DIR=/mnt/psych-bhampstelab/VA_MCI_Rehab/Sean_Working/FS6results
-
-recon-all -make all \
-  -parallel -openmp 2 \
-  -subjid $1
