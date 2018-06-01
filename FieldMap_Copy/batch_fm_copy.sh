fd3409aa7741925d3ba8f5fc656bc81f6e86f002
Sean@benmac <tehsheng+benmac@umich.edu>
2018-06-01 12:58:02 -0400

    changing NPAR=3

--- FieldMap_Copy/batch_fm_copy.sh
+++ FieldMap_Copy/batch_fm_copy.sh
@@ -3,7 +3,7 @@
 # Sean MA, 5/31/2018, 3:50:45 PM
 # inspired by fmri Lab - batch_ummap_fm.sh
 #
-NPAR=1
+NPAR=3
 
 cd /nfs/fmri/RAW_nopreprocess
 SUBJDIRS=$(ls -d hlp17umm007** | grep -v test  | grep -v hlp17umm01494_05012 | sort -t_ -k2)
