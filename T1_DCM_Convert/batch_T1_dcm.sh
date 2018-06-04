b2c49f48098a8fd300fd092950bbd5ec39ffd8f2
Sean@benmac <tehsheng+benmac@umich.edu>
2018-06-04 14:50:52 -0400

    fixed hlp1151 mislabeled folder

--- T1_DCM_Convert/batch_T1_dcm.sh
+++ T1_DCM_Convert/batch_T1_dcm.sh
@@ -13,7 +13,7 @@ cd /nfs/fmri/RAW_nopreprocess
 # find ./{hlp**,bmh**}/dicom/s00003 -type d -exec sh -c 'echo "$(ls {} | wc -l)" {}' \; > t1_dicom_list.txt
 
 # exclude subject folders that have wrong T1 MPRAGE slices: 208
-SUBJDIRS=$(ls -d {hlp**,bmh**} | grep -v hlp17umm01339_03384 | grep -v hlp17umm01405_03782 | grep -v hlp17umm01511_04164 | sort -t_ -k2)
+SUBJDIRS=$(ls -d {hlp**,bmh**} | grep -v hlp17umm01339_03384 | grep -v hlp17umm01405_03782 | grep -v hlp17umm01151_04164 | sort -t_ -k2)
 
 # run shell script thru xargs
 echo $SUBJDIRS | xargs --max-args=1 --max-procs=$NPAR ~/Projects/MADC-FileIO/T1_DCM_Convert/T1_dcm.sh
