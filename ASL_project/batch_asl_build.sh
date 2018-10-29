841d1aefea4438996ca2609e15a88c26ca3b6aee
Sean@benmac <tehsheng+benmac@umich.edu>
2018-10-29 15:47:56 -0400

    initial commit batch for asl_build

---
+++ ASL_project/batch_asl_build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+NPAR=1
+
+cd /nfs/fmri/RAW_nopreprocess
+
+SUBJDIRS=(
+hlp17umm01220_03783
+hlp17umm01243_04201
+hlp17umm01263_03874
+hlp17umm01276_03938
+hlp17umm01389_03696
+hlp17umm01446_04476
+hlp17umm01447_04475
+hlp17umm01458_04524
+hlp17umm01462_04510
+hlp17umm01487_04682
+)
+
+echo $SUBJDIRS | xargs --max-args=1 --max-procs=$NPAR /home/tehsheng/Projects/MADC-FileIO/ASL_project/asl_build.sh
