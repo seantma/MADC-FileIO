1e71f4dd04038b48e16a0d613643df2bede172b4
Sean@benmac <tehsheng+benmac@umich.edu>
2018-10-29 15:44:53 -0400

    initial commit for asl building

---
+++ ASL_project/asl_build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+SUBJ=$1
+echo $SUBJ
+
+# define RAW folders
+RAW=/nfs/fmri/RAW_nopreprocess
+WORK=/nfs/fmri/Analysis/Sean_Working
+
+# make asl folder
+mkdir $RAW/$SUBJ/vasc_3dasl
+
+# transform dicoms
+cd $RAW/$SUBJ/dicom/s00013
+/opt/apps/dcm2niix -f vasc_3dasl .
+
+# move asl files
+mv vasc_3dasl.* $RAW/$SUBJ/vasc_3dasl
+
+# setup analysis files
+mkdir $WORK/ASL_pilot/$SUBJ
+cd mkdir $WORK/ASL_pilot/$SUBJ
+
+cp -p $RAW/$SUBJ/vasc_3dasl/vasc_3dasl.nii .
+cp -p $RAW/$SUBJ/t1mprage_208/t1mprage_208.nii .
+
+# Done
+echo "Finished: " $(SUBJ)
