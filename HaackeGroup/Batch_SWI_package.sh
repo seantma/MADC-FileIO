07627466e7d117c48a162fcfb4549031057fbcaf
Sean@benmac <tehsheng+benmac@umich.edu>
2018-12-04 13:19:57 -0500

    select last 135 subjects to process

--- HaackeGroup/Batch_SWI_package.sh
+++ HaackeGroup/Batch_SWI_package.sh
@@ -11,7 +11,8 @@ NPAR=1
 # sorting by fMRI exam_no
 # processing the last 135 subjects (as the 1st 50 subjects is done)
 cd /nfs/fmri/RAW_nopreprocess
-SUBJ_FILE=$(ls -d {bmh**,hlp**} | grep -v 'PCN' | grep -v 'NOTE' | sort -t_ -k2 )
+SUBJ_FILE=$(ls -d {bmh**,hlp**} | grep -v 'PCN' | grep -v 'NOTE' | sort -t_ -k2
+\ tail -135)
 
 # masked out `--max-args=1` `--max-procs=$NPAR` for OSX
 # cd /Users/tehsheng/Projects/fmri-tools/fMRI-Lab_Blueray_script
