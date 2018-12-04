a020604c5affd077ee39dd24eb7de446ddd3dbce
Sean@benmac <tehsheng+benmac@umich.edu>
2018-12-04 11:12:42 -0500

    FIX home folder bug

--- HaackeGroup/Batch_SWI_package.sh
+++ HaackeGroup/Batch_SWI_package.sh
@@ -13,4 +13,4 @@ SUBJ_FILE=$(ls -d {bmh**,hlp**} | grep -v 'PCN' | grep -v 'NOTE')
 
 # masked out `--max-args=1` `--max-procs=$NPAR` for OSX
 # cd /Users/tehsheng/Projects/fmri-tools/fMRI-Lab_Blueray_script
-echo $SUBJ_FILE | xargs -n1 -P $NPAR /${PWD}/SWI_package.sh 2>&1 | tee UMMAP_SWI_Dicom_List.txt
+echo $SUBJ_FILE | xargs -n1 -P $NPAR ${HOME_DIR}/SWI_package.sh 2>&1 | tee UMMAP_SWI_Dicom_List.txt
