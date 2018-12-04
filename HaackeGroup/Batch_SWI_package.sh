63abc6b70ff76e6aa4b86c03c0325b4e48d0cd5b
Sean@benmac <tehsheng+benmac@umich.edu>
2018-12-04 11:11:54 -0500

    add script home folder

--- HaackeGroup/Batch_SWI_package.sh
+++ HaackeGroup/Batch_SWI_package.sh
@@ -3,6 +3,7 @@
 # Sean MA, 11/13/2018, 2:17:56 AM
 # inspired by fmri Lab - batch_ummap_fm.sh
 #
+HOME_DIR=${PWD}
 NPAR=1
 
 # change to path where md5sum files are located
@@ -12,4 +13,4 @@ SUBJ_FILE=$(ls -d {bmh**,hlp**} | grep -v 'PCN' | grep -v 'NOTE')
 
 # masked out `--max-args=1` `--max-procs=$NPAR` for OSX
 # cd /Users/tehsheng/Projects/fmri-tools/fMRI-Lab_Blueray_script
-echo $SUBJ_FILE | xargs -n1 -P $NPAR ./SWI_package.sh 2>&1 | tee UMMAP_SWI_Dicom_List.txt
+echo $SUBJ_FILE | xargs -n1 -P $NPAR /${PWD}/SWI_package.sh 2>&1 | tee UMMAP_SWI_Dicom_List.txt
