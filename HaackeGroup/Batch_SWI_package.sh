8b458ff0372f032e5a7d24a5e0c2ad027cfee994
Sean@benmac <tehsheng+benmac@umich.edu>
2018-12-04 13:58:18 -0500

    changed to test subjects

--- HaackeGroup/Batch_SWI_package.sh
+++ HaackeGroup/Batch_SWI_package.sh
@@ -12,10 +12,10 @@ NPAR=1
 # processing the last 135 subjects (as the 1st 50 subjects is done)
 cd /nfs/fmri/RAW_nopreprocess
 # SUBJ_FILE=$(ls -d {bmh**,hlp**} | grep -v 'PCN' | grep -v 'NOTE' | sort -t_ -k2 | tail -135)
-# SUBJ_FILE=$(ls -d hlp17umm0154*)  #testing purpose
-SUBJ_FILE=$(ls -d {bmh**,hlp**} | grep -v 'PCN' | grep -v 'NOTE' | sort -t_ -k2)
+SUBJ_FILE=$(ls -d hlp17umm0154*)  #testing purpose
+# SUBJ_FILE=$(ls -d {bmh**,hlp**} | grep -v 'PCN' | grep -v 'NOTE' | sort -t_ -k2)
 
 
 # masked out `--max-args=1` `--max-procs=$NPAR` for OSX
 # cd /Users/tehsheng/Projects/fmri-tools/fMRI-Lab_Blueray_script
-echo $SUBJ_FILE | xargs -n1 -P $NPAR ${HOME_DIR}/SWI_package.sh 2>&1 | tee UMMAP_SWI_Dicom_List__$(date +"%Y%m%d").txt
+echo $SUBJ_FILE | xargs -n1 -P $NPAR ${HOME_DIR}/SWI_package.sh 2>&1 | tee UMMAP_SWI_Dicom_List_$(date +"%Y%m%d").txt
