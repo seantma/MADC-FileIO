d22449a14d7591a186581ed8578e9b39468482cd
Sean@benmac <tehsheng+benmac@umich.edu>
2018-12-04 14:33:42 -0500

    getting dicom list for the lastest 135 subjects

--- HaackeGroup/Batch_SWI_package.sh
+++ HaackeGroup/Batch_SWI_package.sh
@@ -4,18 +4,18 @@
 # inspired by fmri Lab - batch_ummap_fm.sh
 #
 HOME_DIR=${PWD}
-NPAR=5
+NPAR=1
 
 # change to path where md5sum files are located
 # excluding PCN & ScannerSoftwareUpdate_NOTE folders
 # sorting by fMRI exam_no
 # processing the last 135 subjects (as the 1st 50 subjects is done)
 cd /nfs/fmri/RAW_nopreprocess
-# SUBJ_FILE=$(ls -d {bmh**,hlp**} | grep -v 'PCN' | grep -v 'NOTE' | sort -t_ -k2 | tail -135)
-SUBJ_FILE=$(ls -d hlp17umm0154*)  #testing purpose
-# SUBJ_FILE=$(ls -d {bmh**,hlp**} | grep -v 'PCN' | grep -v 'NOTE' | sort -t_ -k2)
+SUBJ_FILE=$(ls -d {bmh**,hlp**} | grep -v 'PCN' | grep -v 'NOTE' | sort -t_ -k2 | tail -135)
+# SUBJ_FILE=$(ls -d hlp17umm0154*)  #testing purpose
+# SUBJ_FILE=$(ls -d {bmh**,hlp**} | grep -v 'PCN' | grep -v 'NOTE' | sort -t_ -k2) #all subjects
 
 
 # masked out `--max-args=1` `--max-procs=$NPAR` for OSX
 # cd /Users/tehsheng/Projects/fmri-tools/fMRI-Lab_Blueray_script
-echo $SUBJ_FILE | xargs -n1 -P $NPAR ${HOME_DIR}/SWI_package.sh 2>&1 | tee UMMAP_SWI_Dicom_List_$(date +"%Y%m%d").txt
+echo $SUBJ_FILE | xargs -n1 -P $NPAR ${HOME_DIR}/SWI_package.sh 2>&1 | tee UMMAP_SWI_Dicom_List_135_$(date +"%Y%m%d").txt
