27a388c13b217f19a47a75af1560514609768267
Sean@benmac <tehsheng+benmac@umich.edu>
2018-12-04 13:19:26 -0500

    sort by exam_no; selected last 135 subjects to process

--- HaackeGroup/Batch_SWI_package.sh
+++ HaackeGroup/Batch_SWI_package.sh
@@ -8,8 +8,10 @@ NPAR=1
 
 # change to path where md5sum files are located
 # excluding PCN & ScannerSoftwareUpdate_NOTE folders
+# sorting by fMRI exam_no
+# processing the last 135 subjects (as the 1st 50 subjects is done)
 cd /nfs/fmri/RAW_nopreprocess
-SUBJ_FILE=$(ls -d {bmh**,hlp**} | grep -v 'PCN' | grep -v 'NOTE')
+SUBJ_FILE=$(ls -d {bmh**,hlp**} | grep -v 'PCN' | grep -v 'NOTE' | sort -t_ -k2 )
 
 # masked out `--max-args=1` `--max-procs=$NPAR` for OSX
 # cd /Users/tehsheng/Projects/fmri-tools/fMRI-Lab_Blueray_script
