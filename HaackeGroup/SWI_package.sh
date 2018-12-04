5703abf199eb458fae26597ad60bb5e449a28f9b
Sean@benmac <tehsheng+benmac@umich.edu>
2018-12-04 09:07:55 -0500

    initial commit of skeleton for SWI package script

---
+++ HaackeGroup/SWI_package.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Sean Ma
+# 12/4/2018
+
+# 1. read `raw_data_cross_reference.dat`
+
+# 2. grep an array of key modalities {t1, t2flair, t2sag, swi_B1, swi_B2, swi_B3, 2dfq}
+
+# 3. echo subject and echo status of modality
+
+ 
