b3973143474508f1e3d7ccbf8ad3b9d4ec1815dd
Sean@benmac <tehsheng+benmac@umich.edu>
2018-12-04 14:14:07 -0500

    remove tar -c

--- HaackeGroup/SWI_package.sh
+++ HaackeGroup/SWI_package.sh
@@ -49,7 +49,7 @@ do
   # echo out
   echo "${SUBJ},${mod},${targetS}"
   # tar it with append; replacing dicom s folders into actual modality
-  tar --append -cvf ${tarfolder}/${SUBJ}.tar --totals ../dicom/${targetS}/* \
+  tar --append -vf ${tarfolder}/${SUBJ}.tar --totals ../dicom/${targetS}/* \
   # --transform='flags=r;s/'${targetS}'/'${mod}'/' \
   # ../dicom/${targetS}/ \
   &> ${tarfolder}/Log_${SUBJ}_$(date +"%Y%m%d").txt
